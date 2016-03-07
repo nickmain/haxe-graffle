9f438da95e6dbfe5a4efcab2c19ad37216bffd52
Nick Main <nickmain@Nicks-iMac.home>
2016-03-06 19:48:53 -0800

    document metadata unit tests

--- src/tests/GraffleMetadataTests.hx
+++ src/tests/GraffleMetadataTests.hx
@@ -18,8 +18,49 @@ class GraffleMetadataTests extends TestCase {
         diagram = loader.diagram;
     }
 
-    public function testBasic() {
-        assertEquals("A", "A");
-        trace(diagram);
+    public function testComments() {
+        assertEquals("These are some comments", diagram.comments);
+    }
+
+    public function testSubject() {
+        assertEquals("hello world", diagram.subject);
+    }
+
+    public function testCopyright() {
+        assertEquals("copywrong", diagram.copyright);
+    }
+
+    public function testVersion() {
+        assertEquals("1.0 Golden Master", diagram.version);
+    }
+
+    public function testDescription() {
+        assertEquals("test diagram", diagram.description);
+    }
+
+    public function testAuthors() {
+        assertEquals("foo author", diagram.authors[0]);
+        assertEquals("bar author", diagram.authors[1]);
+    }
+
+    public function testOrganizations() {
+        assertEquals("orgalorg", diagram.organizations[0]);
+        assertEquals(1, diagram.organizations.length);
+    }
+
+    public function testLanguages() {
+        assertEquals("english", diagram.languages[0]);
+        assertEquals(1, diagram.languages.length);
+    }
+
+    public function testKeywords() {
+        assertEquals("foo", diagram.keywords[0]);
+        assertEquals("bar", diagram.keywords[1]);
+        assertEquals("baz", diagram.keywords[2]);
+    }
+
+    public function testProjects() {
+        assertEquals("haxe-graffle", diagram.projects[0]);
+        assertEquals(1, diagram.projects.length);
     }
 }
